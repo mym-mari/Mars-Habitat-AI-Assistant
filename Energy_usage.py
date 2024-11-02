baa9f16ae32eb53def2eac565c1c48de5304fc6a
Mariana Mendez <marianamendez200515@gmail.com>
2024-11-01 22:44:37 -0500

    energy-temperature-oxygen usage commit

--- Energy_usage.py
+++ Energy_usage.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt 
 import time 
 INTERVAL = 1
@@ -11,19 +12,21 @@ IMPORTING_INTERVAL = 10
 #filename = una variable tipo string
 
 class ai_mars: #class ai, monitors the data, it saves it each timestep into a csv file and predicts new data
-    def __init__(self, max_value, variable, filename, position):
+    def __init__(self, max_value, variable, filename, position, model_type, n_estimators):
         self.max_value = max_value
         self.variable = variable 
         self.filename = filename + '.csv'
         self.position = position
+        self.model_type = model_type
+        self.n_estimators = n_estimators
         
         self.data = pd.read_csv("/mnt/c/Users/maria/Desktop/Cuarto semestre/Programación/monitoring_data.csv", usecols=['time', self.variable])
         #for general use, put in the first argument the location of the file (that i will attach in the mail)
+        
         self.df_variable = self.data.iloc[::position] #takes the variable of interest and storages it 
         
         self.df_results = pd.DataFrame(columns= ['time',self.variable]) #empty columns for later adding data, empty dataframe
         
-        self.model = LinearRegression() #calls a function of linear regression
         self.predictions = [] #creates an empty array for predictions in the variable of interest
         self.prediction_times = [] #creates an empty array for time
         
@@ -34,15 +37,26 @@ class ai_mars: #class ai, monitors the data, it saves it each timestep into a cs
            
            if fila [self.variable] > self.max_value: #conditional for checking if the value exceeds certain number
                print(f'Exceeded {self.variable} in {fila[self.variable]} at {fila["time"]}') #prints the exceeded value at a certain moment
+               
            new_row = pd.DataFrame([[fila['time'], fila[self.variable]]], columns=['time', self.variable]) #creates a new row, of a dataframe with the analyzed values
            self.df_results = pd.concat([self.df_results, new_row], ignore_index=True) #adds the row to the empty array
+           
+           #ADDING A PREDICTIVE MODEL ai helped with the aproximation
+           if self.model_type == 'forest':
+               self.model = RandomForestRegressor(n_estimators=self.n_estimators, random_state=42) #calls a function of random forests
+           else:    
+               self.model = LinearRegression() #calls a function of linear regression
+          
+           
            
            X = self.df_variable['time'].values.reshape(-1, 1)
            y = self.df_variable[self.variable].values
            self.model.fit(X, y)
-           next_time = fila['time'] + 1  # Ajusta según corresponda tu intervalo de tiempo
+           
+           #addind the information to the arrays
+           next_time = fila['time'] + 0.01  
            prediction = self.model.predict(np.array([[next_time]]))[0]
-           self.predictions.append(prediction)
+           self.predictions.append(prediction) 
            self.prediction_times.append(next_time)
            
            
@@ -62,15 +76,7 @@ class ai_mars: #class ai, monitors the data, it saves it each timestep into a cs
        print(self.df_results)
        self.plotting()
        
-       
-
-    def absolute_manual_error(self):
-        y_true = self.df_variable[self.variable].values
-        y_pred = self.model.predict(self.df_variable['time'].values.reshape(-1, 1))
-        manual_mae = sum(abs(y_true - y_pred)) / len(y_true)
-        print(f"Error absoluto medio manual: {manual_mae}")
-    
-        
+            
     def plotting(self):
        plt.figure(figsize=(10, 5))
        plt.plot(self.df_results['time'], self.df_results[self.variable], label='Actual Data', color='blue')
@@ -83,7 +89,7 @@ class ai_mars: #class ai, monitors the data, it saves it each timestep into a cs
 
 
 
-data1 = ai_mars(max_value = 4.0, variable = 'temperature', filename = 'temperature_results', position = 2)
+data1 = ai_mars(max_value = 4.0, variable = 'temperature', filename = 'temperature_results', position = 2, model_type = 'forest', n_estimators= 200)
 data1.storing_data()
 data1.absolute_manual_error()
 data1.plotting()
